08ab26cd8198391fcaa096935df770810c2dddca
Alex Maguey <alexmaguey1@gmail.com>
2017-07-04 00:26:58 -0500

    added post new

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -2,9 +2,10 @@ import _ from 'lodash';
 import { FETCH_POSTS } from '../actions';
 
 export default function(state = {}, action) {
-  switch (action) {
-    case expression:
+  switch (action.type) {
+    case FETCH_POSTS:
       return _.mapKeys(action.payload.data, 'id');
+    
     default:
       return state;
   }
